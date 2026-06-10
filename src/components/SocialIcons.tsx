ea34e068ba9e8d5194c99604e4df4d82219d584c
Ilisha Dabas <ilisha@Ilishas-MacBook-Air.local>
2026-06-10 20:47:09 +0530

    Updated portfolio content

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -11,7 +11,7 @@ const SocialIcons = () => {
   useEffect(() => {
     const social = document.getElementById("social") as HTMLElement;
 
-    social.querySelectorAll("span").forEach((item) => {
+    social.querySelectorAll(".social-icon-item").forEach((item) => {
       const elem = item as HTMLElement;
       const link = elem.querySelector("a") as HTMLElement;
 
@@ -57,31 +57,31 @@ const SocialIcons = () => {
   return (
     <div className="icons-section">
       <div className="social-icons" data-cursor="icons" id="social">
-        <span>
-          <a
-            href="https://www.linkedin.com/in/ilisha-dabas-84b07b27b/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaLinkedinIn />
-          </a>
-        </span>
-        <span>
-          <a
-            href="https://www.behance.net/ilishadabas"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaBehance />
-          </a>
-        </span>
+      <span className="social-icon-item">
+      <a
+        href="https://www.linkedin.com/in/ilisha-dabas-84b07b27b/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaLinkedinIn />
+      </a>
+    </span>
+    <span className="social-icon-item">
+      <a
+        href="https://www.behance.net/ilishadabas"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FaBehance />
+      </a>
+    </span>
       <a
         className="CV-button"
         href="https://drive.google.com/file/d/1x9Y4QfGjFj7ddupg1h2GsKWx_swkJu1B/view?usp=sharing"
         target="_blank"
         rel="noreferrer"
       >
-        <HoverLinks text="RESUME" />
+        <HoverLinks text="MY CV" />
         <span>
           <TbNotes />
         </span>
